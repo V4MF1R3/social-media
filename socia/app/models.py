e489e301eac1cf4cda7a56281971c29263bd1850
V4MF1R3 <pantvaibhav16@gmail.com>
2024-03-11 14:40:53 +0530

    created custom user model and manager

---
+++ socia/app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from app.manager import UserManager
+
+class User(AbstractUser):
+
+    username = None
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=16)
+
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = UserManager()
